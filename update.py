f7e7b6801418fec4c766686e9fb79fce8da73457
Sridhar Cuddalore Parthasarathy <scp392@nyu.edu>
2019-04-05 19:45:27 -0600

    Add files via upload

--- update.py
+++ update.py
@@ -19,17 +19,26 @@ changes = {
     "ClearLearnedFaceById(" : "ForgetFace(",
     "ClearLearnedFaces(": "ForgetAllFaces(",
     "GetLearnedFaces(" : "GetKnownFaces(",
+    "_GetLearnedFaces(" : "_GetKnownFaces(",
     "ChangeDisplayImage(" : "DisplayImage(",
     "GetListOfImages(" : "GetImageList(",
+    "_GetListOfImages(" : "_GetImageList(",
     "SaveImageAssetToRobot(" : "SaveImage(",
+    "_SaveImageAssetToRobot(" : "_SaveImage(",
     "DeleteImageAssetFromRobot(" : "DeleteImage(",
     "GetListOfAudioFiles(" : "GetAudioList(",
+    "_GetListOfAudioFiles(" : "_GetAudioList(",
     "PlayAudioClip(" : "PlayAudio(",
     "SaveAudioAssetToRobot(" : "SaveAudio(",
+    "_SaveAudioAssetToRobot(" : "_SaveAudio(",
     "DeleteAudioAssetFromRobot(" : "DeleteAudio(",
+    "_DeleteAudioAssetFromRobot(" : "_DeleteAudio(",
     "SlamGetMap(" : "GetMap(",
+    "_SlamGetMap(" : "_GetMap(", 
     "SlamGetPath(" : "GetSlamPath(",
+    "_SlamGetPath(" : "_GetSlamPath(",
     "SlamGetSensorSerialNumber(" : "GetSlamSerialNumber(",
+    "_SlamGetSensorSerialNumber(" : "_GetSlamSerialNumber(",
     "SlamGetStatus(" : "GetSlamStatus(",
     "SlamReset(" : "ResetSlam(",
     "SlamSensorReboot(" : "RebootSlam(",
@@ -56,20 +65,39 @@ changes = {
     "_StringMessage(" : "_SerialMessage("
 }
 
-# print(changes)
-# print([value for key, value in changes.items() if 'ChangeDisplayImage(' in key][0])
+# SetHeadPostion => MoveHeadPosition updated commands are not indended right - you could try to fix that !
+def setHeadSupporter(axis,line,file):
+    dataChunks = line.split(',')
+    value = dataChunks[1]
+    if axis is "roll":
+        line = line.replace(line,"misty.MoveHeadPosition(null,"+str(value)+",null);") 
+    elif axis is "pitch":
+        line = line.replace(line,"misty.MoveHeadPosition("+str(value)+",null,null);") 
+    else:
+        line = line.replace(line,"misty.MoveHeadPosition(null,null,"+str(value)+");") 
+    file.write(line+"\n")
 
 for skillFile in jsFiles:
     with open(skillFile,'r') as dataFile:
         data = dataFile.read()
-        # print(data)
         for key, value in changes.items():
             data = data.replace(key, value)
-        # print(data)
-        newFile = open('updated_'+skillFile, "x")
-        newFile.write(data)
-        newFile.close()
+        tempFile = open('temp_'+skillFile, "x")
+        tempFile.write(data)
+        tempFile.close()
         dataFile.close()
-
-
+    with open('temp_'+skillFile,'r') as dataFile:
+        newFile = open('updated_'+skillFile, "x")
+        for line in dataFile.readlines():
+            if "misty.SetHeadPosition(\"roll\"" in line:
+                setHeadSupporter("roll",line,newFile)
+            elif "misty.SetHeadPosition(\"pitch\"" in line:
+                setHeadSupporter("pitch",line,newFile)
+            elif "misty.SetHeadPosition(\"yaw\"" in line:
+                setHeadSupporter("yaw",line,newFile)
+            else:
+                newFile.write(line)
+        os.remove('temp_'+skillFile)
+        newFile.close()
+        
 print("Hello Misty!")
